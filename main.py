1f1b373ed79c67bca2556d5e3a82f280ad7e8191
Matt Megarry <mattmegarry@gmail.com>
2019-09-16 14:15:28 +0100

    Use a function and a string method

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 print("Welcome to Python Play, an almost totally useless program (from the users point of view)...")
 
-username = input("What's your name?\n")
+def getUserName():
+  username = input("What's your name?\n")
+  if username == "":
+    username = input("No but really, TELL ME your name!\n")
+  print("Hi " + username + ", great to meet you!")
+  return username
 
-if username == "":
-  username = input("No but really, TELL ME your name!\n")
-
-print("Hi " + username + ", great to meet you!")
+username = getUserName()
+print("Your username is " + str(len(username)) + " characters")
+print("Your username is {} characters".format(len(username)))
