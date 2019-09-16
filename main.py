94da23879ccd66ff1db5c10e305b48a8fa1bd59f
Matt Megarry <mattmegarry@gmail.com>
2019-09-16 13:58:20 +0100

    Take some user input from the command line and show it to to the user

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to Python Play, an almost totally useless program (from the users point of view)...")
+
+username = input("What's your name?\n")
+
+if username == "":
+  username = input("No but really, TELL ME your name!\n")
+
+print("Hi " + username + ", great to meet you!")
